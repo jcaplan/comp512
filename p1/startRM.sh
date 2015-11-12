04ee561f46d9aee5bc5b56813a71e233884775ff
jcaplan <jonah.caplan@mail.mcgill.ca>
2015-11-11 19:11:50 -0500

    report

--- p1/startRM.sh
+++ p1/startRM.sh
@@ -4,16 +4,16 @@
 #ports numbered successively from middleware port 18080
 
 xterm -e ant server -Dserver-service.name=rmCar -Dserver-service.port=28081 \
-	-Dserver-shutdown.port=28085 &
+	-Dserver-shutdown.port=28085 > rm1.txt &
 sleep 5
 xterm -e ant server -Dserver-service.name=rmRoom -Dserver-service.port=28082 \
-	-Dserver-shutdown.port=28087 &
+	-Dserver-shutdown.port=28087 > rm2.txt &
 sleep 5
 xterm -e ant server -Dserver-service.name=rmFlight -Dserver-service.port=28083 \
-	-Dserver-shutdown.port=28086 &
+	-Dserver-shutdown.port=28086 > rm3.txt &
 sleep 5
 xterm -e ant server -Dserver-service.name=rmCustomer -Dserver-service.port=28084 \
-	-Dserver-shutdown.port=28088 &
+	-Dserver-shutdown.port=28088 > rm4.txt &
 
 # xterm -e ant server -Dserver-service.name=rmRoom -Dserver-service.port=18082 &
 # sleep 2
